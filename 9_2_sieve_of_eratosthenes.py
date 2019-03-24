3a07f23851b23ea2ae72ce7fb9b0ade8ef7a322f
domodomodomo <>
2019-03-25 02:59:37 +0900

    debug

--- 9_2_sieve_of_eratosthenes.py
+++ 9_2_sieve_of_eratosthenes.py
@@ -11,10 +11,10 @@ def comparison():
         primes_ordered_dict
     ]
     funcscale.argument_list = [
-        ((10**i, ), {}) for i in range(1, 5)
+        ((10**i, ), {}) for i in range(2, 5)
     ]
     funcscale.str_argument_list = [
-        f'(10**{i}, )' for i in range(1, 5)
+        f'10**{i}' for i in range(2, 5)
     ]
     funcscale.compare()
 
@@ -41,41 +41,6 @@ def primes_fast(n):
     return [i for i in range(n + 1) if is_prime[i]]
 
 
-def primes_ordered_dict(n):
-    #
-    prime_list = []
-    non_prime_list = []
-    number_dict = collections.OrderedDict(
-        ((i, None) for i in range(n + 1)))
-    #
-    del number_dict[0]
-    del number_dict[1]
-    while True:
-        # get prime
-        prime, _ = number_dict.popitem(False)
-        prime_list.append(prime)
-        # break
-        if prime * prime > n:
-            break
-        # get non primes
-        non_prime_list.append(prime * prime)
-        for k in number_dict:
-            if prime * k <= n:
-                non_prime_list.append(prime * k)
-            else:
-                break
-        # delete non primes
-        for non_prime in non_prime_list:
-            del number_dict[non_prime]
-        non_prime_list.clear()
-
-    # add rest of numbers as prime
-    prime_list.extend(
-        [prime for prime, _ in number_dict.items()])
-
-    return prime_list
-
-
 def primes_dict(n):
     #
     prime_list = []
@@ -90,7 +55,7 @@ def primes_dict(n):
         # get prime
         prime, _ = number_dict.popitem()
         prime_list.append(prime)
-        if prime * prime > n:
+        if not (prime * prime <= n):
             break
 
         # get non primes
@@ -112,6 +77,41 @@ def primes_dict(n):
     while number_dict:
         prime, _ = number_dict.popitem()
         prime_list.append(prime)
+
+    return prime_list
+
+
+def primes_ordered_dict(n):
+    #
+    prime_list = []
+    non_prime_list = []
+    number_dict = collections.OrderedDict(
+        ((i, None) for i in range(n + 1)))
+    #
+    del number_dict[0]
+    del number_dict[1]
+    while True:
+        # get prime
+        prime, _ = number_dict.popitem(False)
+        prime_list.append(prime)
+        # break
+        if not (prime * prime <= n):
+            break
+        # get non primes
+        non_prime_list.append(prime * prime)
+        for k in number_dict:
+            if prime * k <= n:
+                non_prime_list.append(prime * k)
+            else:
+                break
+        # delete non primes
+        for non_prime in non_prime_list:
+            del number_dict[non_prime]
+        non_prime_list.clear()
+
+    # add rest of numbers as prime
+    prime_list.extend(
+        [prime for prime, _ in number_dict.items()])
 
     return prime_list
 
